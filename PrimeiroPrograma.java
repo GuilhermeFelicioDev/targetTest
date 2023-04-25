b53828064722cc55676123e00066f69a845941e8
GuilhermeWarlord <gfcwarlord@gmail.com>
2023-04-25 20:08:58 -0300

    Add files via upload
    Código da resolução do primeiro teste.

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+public class PrimeiroPrograma {
+    public static void main(String[] args) throws Exception {
+            int indice = 13, soma = 0, k = 0;
+            while( k < indice){
+            k++;
+            soma = soma + k;
+            }
+        System.out.println(soma);
+    }
+}
